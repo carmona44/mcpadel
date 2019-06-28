c306872a5a36ea076e09ccbf3e28cff982ddb9fd
carmona44 <daniel_95ca@hotmail.com>
2019-06-28 11:28:55 +0200

    Updates

---
+++ precache-manifest.08c0d0bf0e03b0c389dff185e9ff8c44.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0e55096cf080c8333b2caf0c72ddbc1",
+    "url": "/mcpadel/index.html"
+  },
+  {
+    "revision": "f95e2046278928e56037",
+    "url": "/mcpadel/static/css/2.30463588.chunk.css"
+  },
+  {
+    "revision": "c9f7278c3214caa725b1",
+    "url": "/mcpadel/static/css/main.0c5d491c.chunk.css"
+  },
+  {
+    "revision": "f95e2046278928e56037",
+    "url": "/mcpadel/static/js/2.12081648.chunk.js"
+  },
+  {
+    "revision": "c9f7278c3214caa725b1",
+    "url": "/mcpadel/static/js/main.23723dd4.chunk.js"
+  },
+  {
+    "revision": "d85147773896a11b34e0",
+    "url": "/mcpadel/static/js/runtime~main.58f67504.js"
+  },
+  {
+    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
+    "url": "/mcpadel/static/media/Roboto-Bold.39b2c303.woff2"
+  },
+  {
+    "revision": "dc81817def276b4f21395f7ea5e88dcd",
+    "url": "/mcpadel/static/media/Roboto-Bold.dc81817d.woff"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/mcpadel/static/media/Roboto-Bold.e31fcf18.ttf"
+  },
+  {
+    "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
+    "url": "/mcpadel/static/media/Roboto-Bold.ecdd509c.eot"
+  },
+  {
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "/mcpadel/static/media/Roboto-Light.3b813c2a.woff"
+  },
+  {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/mcpadel/static/media/Roboto-Light.46e48ce0.ttf"
+  },
+  {
+    "revision": "69f8a0617ac472f78e45841323a3df9e",
+    "url": "/mcpadel/static/media/Roboto-Light.69f8a061.woff2"
+  },
+  {
+    "revision": "a990f611f2305dc12965f186c2ef2690",
+    "url": "/mcpadel/static/media/Roboto-Light.a990f611.eot"
+  },
+  {
+    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
+    "url": "/mcpadel/static/media/Roboto-Medium.4d9f3f9e.eot"
+  },
+  {
+    "revision": "574fd0b50367f886d359e8264938fc37",
+    "url": "/mcpadel/static/media/Roboto-Medium.574fd0b5.woff2"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/mcpadel/static/media/Roboto-Medium.894a2ede.ttf"
+  },
+  {
+    "revision": "fc78759e93a6cac50458610e3d9d63a0",
+    "url": "/mcpadel/static/media/Roboto-Medium.fc78759e.woff"
+  },
+  {
+    "revision": "2751ee43015f9884c3642f103b7f70c9",
+    "url": "/mcpadel/static/media/Roboto-Regular.2751ee43.woff2"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "/mcpadel/static/media/Roboto-Regular.30799efa.eot"
+  },
+  {
+    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
+    "url": "/mcpadel/static/media/Roboto-Regular.ba3dcd89.woff"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/mcpadel/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "7500519de3d82e33d1587f8042e2afcb",
+    "url": "/mcpadel/static/media/Roboto-Thin.7500519d.woff"
+  },
+  {
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/mcpadel/static/media/Roboto-Thin.94998475.ttf"
+  },
+  {
+    "revision": "954bbdeb86483e4ffea00c4591530ece",
+    "url": "/mcpadel/static/media/Roboto-Thin.954bbdeb.woff2"
+  },
+  {
+    "revision": "dfe56a876d0282555d1e2458e278060f",
+    "url": "/mcpadel/static/media/Roboto-Thin.dfe56a87.eot"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/mcpadel/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/mcpadel/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "38975343b586296e9b73e6b56cc3ec5d",
+    "url": "/mcpadel/static/media/fa-brands-400.38975343.svg"
+  },
+  {
+    "revision": "8e49b728413079dfd9ee45d0c58d54e4",
+    "url": "/mcpadel/static/media/fa-brands-400.8e49b728.eot"
+  },
+  {
+    "revision": "9f018d1025561e96439f7c0e9026301a",
+    "url": "/mcpadel/static/media/fa-brands-400.9f018d10.woff"
+  },
+  {
+    "revision": "9f4ce3dc689981a1b87faab0f5484f9e",
+    "url": "/mcpadel/static/media/fa-brands-400.9f4ce3dc.woff2"
+  },
+  {
+    "revision": "b7d071b9c3c197bff4af902070622423",
+    "url": "/mcpadel/static/media/fa-brands-400.b7d071b9.ttf"
+  },
+  {
+    "revision": "7980a6361c25b4665dbbe92d4488783c",
+    "url": "/mcpadel/static/media/fa-regular-400.7980a636.woff2"
+  },
+  {
+    "revision": "7aaf5675efd6339e9aba53ecbe5f1e36",
+    "url": "/mcpadel/static/media/fa-regular-400.7aaf5675.woff"
+  },
+  {
+    "revision": "859001f6ae8eb0bb3878aaa971b50fc6",
+    "url": "/mcpadel/static/media/fa-regular-400.859001f6.eot"
+  },
+  {
+    "revision": "da8a235bb207c74eea21507f3a86a53b",
+    "url": "/mcpadel/static/media/fa-regular-400.da8a235b.svg"
+  },
+  {
+    "revision": "f33342516f7cbe46f1d6b68f9e7bbeda",
+    "url": "/mcpadel/static/media/fa-regular-400.f3334251.ttf"
+  },
+  {
+    "revision": "0be94a07755ba9b88f2ebcac0f23a3da",
+    "url": "/mcpadel/static/media/fa-solid-900.0be94a07.woff"
+  },
+  {
+    "revision": "64b3e814a66c2719b15abf8f7998bd73",
+    "url": "/mcpadel/static/media/fa-solid-900.64b3e814.woff2"
+  },
+  {
+    "revision": "7726a281c1d436eb038f78c6e9048c96",
+    "url": "/mcpadel/static/media/fa-solid-900.7726a281.svg"
+  },
+  {
+    "revision": "e2675a616b68f446fa6284c111554c7f",
+    "url": "/mcpadel/static/media/fa-solid-900.e2675a61.eot"
+  },
+  {
+    "revision": "f14c3b2ff7c821a4c838debbffd6ad2d",
+    "url": "/mcpadel/static/media/fa-solid-900.f14c3b2f.ttf"
+  },
+  {
+    "revision": "ddbc8527c9d5fa29660dc9101da0df37",
+    "url": "/mcpadel/static/media/miniparejas1.ddbc8527.png"
+  },
+  {
+    "revision": "6ff00898db29d9700e59ca0f77e1d4ce",
+    "url": "/mcpadel/static/media/miniparejas2.6ff00898.png"
+  },
+  {
+    "revision": "9b58b86d44c76e1017c2e075cd38d550",
+    "url": "/mcpadel/static/media/miniparejas3.9b58b86d.png"
+  },
+  {
+    "revision": "b91591f2938ded720decd21639ae0067",
+    "url": "/mcpadel/static/media/miniparejas4.b91591f2.png"
+  },
+  {
+    "revision": "f9ca0b412901ad4afe0e74e5b1a16a6d",
+    "url": "/mcpadel/static/media/miniparejas5.f9ca0b41.png"
+  },
+  {
+    "revision": "6098fd86b8f15b0f82be52831f5e66e6",
+    "url": "/mcpadel/static/media/miniparejas6.6098fd86.png"
+  },
+  {
+    "revision": "8dd13001985cf952d9cca01b9430c8d4",
+    "url": "/mcpadel/static/media/parejas-jornada1-1.8dd13001.jpg"
+  },
+  {
+    "revision": "81dd88f8d2537f38625fb1d0aea27ced",
+    "url": "/mcpadel/static/media/parejas-jornada1-2.81dd88f8.jpg"
+  },
+  {
+    "revision": "37576875bc1d41d2731657b407f349e5",
+    "url": "/mcpadel/static/media/parejas-jornada1-3.37576875.jpg"
+  }
+]);
